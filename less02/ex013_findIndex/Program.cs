d2e06ea0f523db80f2b13e9369f1f547c8347f93
*Egor <egorik13@gmail.com>
2023-02-25 13:46:39 +0300

    add lection C# 2 - array, random

---
+++ less02/ex013_findIndex/Program.cs
@@ -0,0 +1,16 @@
+﻿//               0   1    2   3   4   5   6  7   8    9
+int[] array = { 11, 211, 31, 41, 18, 15, 61, 17, 18, 19 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ less02/ex014_arrayRandom/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // -1 искуственный прием, выдающий результат -1 при отсуствии искомого элемента.
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+array[5] = 5;
+array[8]=5;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
