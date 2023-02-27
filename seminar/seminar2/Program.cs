cf207a1f2db5d992d6c947e1e2c3adc3a165c691
*Egor <egorik13@gmail.com>
2023-02-27 21:21:54 +0300

    upd: sem2

--- seminar/seminar2/Program.cs
+++ seminar/seminar2/Program.cs
@@ -73,21 +73,14 @@
 // 34, 5 -> не кратно, остаток 4
 // 16, 4  -> кратно
 
+// void Krat(int num1, int num2)
+// {
+//     int res = num1 % num2;
+//     if (res == 0) Console.WriteLine($"Кратно");
+//     else Console.WriteLine($"не кратно, {res}");
+// }   
 
-int Krat(int num1, int num2)
-{
-    int res = num1 % num2;
-    return res;
-}   
-   
-int randomnum1 = new Random().Next(1, 1000);
-int randomnum2 = new Random().Next(1, 1000);
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-//randomnum1 = 16;
-//randomnum2 = 4;
-//Console.WriteLine(randomnum1);
-//Console.WriteLine(randomnum2);
-//Console.WriteLine(Krat(randomnum1, randomnum2));
-
-if (Krat(randomnum1, randomnum2) == 0) Console.WriteLine($"Кратно");
-else Console.WriteLine($"не кратно, {Krat(randomnum1, randomnum2)}");
+// Krat(num1, num2);
